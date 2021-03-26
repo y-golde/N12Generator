d20de1f749504b7fa52a44bdd22e952dea8ba112
y-golde <yon.golde@gmail.com>
2021-03-26 16:16:14 +0300

    removed problematic words

--- src/Formats/formats.ts
+++ src/Formats/formats.ts
@@ -2,14 +2,14 @@ import * as WordGroups from '../Groups';
 
 const formats = () => [
     `
-        <strong>${getRandMember(WordGroups.DRAMATIC_TITLES)}:</strong>  
+        <strong>${getRandMember(WordGroups.DRAMATIC_TITLES)} - </strong>  
         ${getRandMember(WordGroups.MEET_CONNECTORS)} 
         ${getRandMember(WordGroups.GROUPS_OF_PEOPLE)}
         ש${getRandMember(WordGroups.SINGLE_PERSON_ACTIONS)}
         ${Math.random() > 0.3 ? getRandMember(WordGroups.TIMES) : ''}
     `,
     `
-        <strong>${getRandMember(WordGroups.DRAMATIC_TITLES)}:</strong> 
+        <strong>${getRandMember(WordGroups.DRAMATIC_TITLES)} - </strong> 
         ${getRandMember(WordGroups.GROUPS_OF_PEOPLE)} 
         ש${getRandMember(WordGroups.TWO_PERSON_ACTIONS)}
         ${getRandMember(WordGroups.GROUPS_OF_PEOPLE)} 
@@ -18,7 +18,7 @@ const formats = () => [
 
 ]
 
-const getRandMember = (group : string[]) => {
+export const getRandMember = (group : string[]) => {
     return group[Math.floor(Math.random() * group.length)];
 }
 
